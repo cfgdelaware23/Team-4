792174d51650c7cebc32f1fc02ff719d2dd44379
steven-kadavil <109249603+steven-kadavil@users.noreply.github.com>
2023-09-30 00:46:18 -0400

    Update tailwind.config.js

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-<<<<<<< HEAD
-<<<<<<< HEAD
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
@@ -16,19 +14,4 @@ module.exports = {
   },
   plugins: [],
 }
-=======
-=======
->>>>>>> branch
-    "./src/**/*.{js,jsx,ts,tsx}"
-  ],
-  theme: {
-    extend: {
-      colors: {
-        'tan': '#fcf1e3',
-        'ketchup': '#d83c23',
-        'silver': '#6e96a4',
-      }
-    },
-  },
-  plugins: [],
-}
+
