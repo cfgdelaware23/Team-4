d357df41614c4f7c7c1aaddca6f7c7b2d04ec54a
“Steven <spk60569@psu.edu>
2023-09-30 08:25:10 -0400

    route index file

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,41 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import reportWebVitals from './reportWebVitals';
+import Root from "./routes/root";
+import SignUp from './signup';
+import Thanks from './Components/Thanks';
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root/>,
+  },
+  {
+    path: "/sign",
+    element: <SignUp />,
+  },
+  
+  {
+    path: "/thank",
+    element: <Thanks />
+  },
+
+]);
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
 root.render(
   <React.StrictMode>
-    <App />
+     <RouterProvider router={router} />
   </React.StrictMode>
 );
 
