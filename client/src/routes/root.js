1ec1f14679b90c91f3a6dccf6d1fd63374d09083
“Steven <spk60569@psu.edu>
2023-09-30 08:41:38 -0400

    Fixed root.js

--- client/src/routes/root.js
+++ client/src/routes/root.js
@@ -17,12 +17,12 @@ export default function Root(){
        <span className = "text-center text-8xl mt-24"><h1><span className= "text-ketchup">Well</span><span className="text-gray-500">fare </span></h1></span>
     
      
-        <div className=' flex space-x-20'>
+        
         <nav>
-        <button onClick = {clicked} className="w-1/2 rounded-xl border-4 border-black border-solid
+        <button onClick = {clicked} className="w-1/2 h 1/2 rounded-xl border-4 border-black border-solid
             text-tan bg-ketchup font-bold text-4xl gap-right-4"><Link to = "/sign">Signup </Link></button>
         
-        <button onClick = {clicked} className="w-1/2 rounded-xl border-4 border-black border-solid
+        <button onClick = {clicked} className="w-1/2 h 1/3 rounded-xl border-4 border-black border-solid
             text-tan bg-ketchup font-bold text-4xl">       Login
         </button>
         
@@ -32,7 +32,7 @@ export default function Root(){
     
     
     
-        </div>
+   
     
         
     </div>
