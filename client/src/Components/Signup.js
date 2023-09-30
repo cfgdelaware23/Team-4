105eec1b87d1a0366db51beeda5e1ac5494c3d65
“Steven <spk60569@psu.edu>
2023-09-30 09:13:50 -0400

    fix button saying click me

--- client/src/Components/Signup.js
+++ client/src/Components/Signup.js
@@ -2,9 +2,7 @@
 import React from 'react';
 function Signup(){
 
-    function clicked(){
-        alert("You clicked me")
-    }
+   
     return (
         
 
@@ -22,9 +20,9 @@ function Signup(){
                <span className = "text-center text-8xl mt-24"><h1><span className= "text-ketchup">Well</span><span className="text-gray-500">fare </span></h1></span>
 
 
-                <button onClick = {clicked} className="w-1/2 h-1/3 rounded-xl border-4 border-black border-solid
+                <button  className="w-1/2 h-1/3 rounded-xl border-4 border-black border-solid
                     text-tan bg-ketchup font-bold text-4xl">Signup</button>
-                <button onClick = {clicked} className="w-1/2 h h-1/3 rounded-xl border-4 border-black border-solid
+                <button  className="w-1/2 h h-1/3 rounded-xl border-4 border-black border-solid
                     text-tan bg-ketchup font-bold text-4xl">Skip</button>
                 
             </div>
